742408d9050f9fa9f421df206304e49df505cefd
jakkappro <jakkappro@gmail.com>
2023-02-21 09:07:03 +0100

    first commit

---
+++ foodauditServer/src/main/java/com/customdev/org/foodauditServer/FoodauditServerApplication.java
@@ -0,0 +1,13 @@
+package com.customdev.org.foodauditServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodauditServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodauditServerApplication.class, args);
+	}
+
+}
---
+++ foodauditServer/src/test/java/com/customdev/org/foodauditServer/FoodauditServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customdev.org.foodauditServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodauditServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
